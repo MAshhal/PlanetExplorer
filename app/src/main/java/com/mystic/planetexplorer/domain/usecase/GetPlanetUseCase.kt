9c6e0d0c07fefaa0ccd8f27c6e905200156d9edc
Muhammad Ashhal <96419703+MAshhal@users.noreply.github.com>
2025-12-05 12:28:49 +0500

    feat: Add domain layer use cases
    Adds use cases to the domain layer for fetching planet data.
    
    This includes:
    - `GetPlanetUseCase` to retrieve a single planet by its ID.
    - `GetPlanetsUseCase` to fetch a list of all planets.
    
    Both use cases delegate the call to the `PlanetRepository`.

---
+++ app/src/main/java/com/mystic/planetexplorer/domain/usecase/GetPlanetUseCase.kt
@@ -0,0 +1,17 @@
+package com.mystic.planetexplorer.domain.usecase
+
+import com.mystic.planetexplorer.core.model.Planet
+import com.mystic.planetexplorer.core.network.Result
+import com.mystic.planetexplorer.domain.repository.PlanetRepository
+import javax.inject.Inject
+
+/**
+ * Created: Fri 05 Dec 2025
+ * Author: Muhammad Ashhal
+ */
+
+class GetPlanetUseCase @Inject constructor(
+    private val repository: PlanetRepository
+) {
+    suspend operator fun invoke(id: Int): Result<Planet> = repository.getPlanet(id)
+}
